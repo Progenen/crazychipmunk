f371dd17009533979677e4e4b5f600019a239b9f
Leoneliuse <sbekbauov987@gmail.com>
2025-05-01 20:34:00 +0500

    Создан блок header, нужны доработки в бургере

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 })
+
+const burger = document.querySelector(".header__burger"),
+    close = document.querySelector(".header__menu-close"),
+    menu = document.querySelector(".header__menu");
+
+burger.addEventListener("click", () => {
+    menu.classList.add("header__menu_active");
+    document.body.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+    menu.classList.remove("header__menu_active");
+    document.body.style.overflow = "";
+});
